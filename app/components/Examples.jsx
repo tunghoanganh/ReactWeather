493eac37df8d345ea776767e712eeeab711bca49
Tung Hoang <tunghoanganh2310@gmail.com>
2017-01-13 18:53:54 +0800

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,8 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h2>Examples compnent!</h2>
+    <h3>Examples compnent!</h3>
+    <p>Welcome to Examples page.</p>
   );
 };
 
